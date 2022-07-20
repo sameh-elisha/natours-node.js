84a8e681ad5be79c29ecf240a413138daa2b615f
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-07-20 05:14:35 +0200

    Update apiFeatures.js

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -5,59 +5,49 @@ class APIFeatures {
   }
 
   filter() {
-    // Take shadow copy
-    const objQuery = { ...this.queryString };
-    const excludedFields = ['sort', 'page', 'limit', 'fields'];
-    excludedFields.forEach(elm => delete objQuery[elm]);
-    // Json to string
-    let filterString = JSON.stringify(objQuery);
-    // Add $ before this words to make filter work
-    filterString = filterString.replace(
-      /\b(lte|lt|gt|gte)\b/g,
-      match => `$${match}`
-    );
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
-    this.query = this.query.find(JSON.parse(filterString));
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
     return this;
   }
 
   sort() {
-    // Sorting
-    // Check is there sort object
     if (this.queryString.sort) {
-      // {sort: "price,avg"}
-      const sort = this.queryString.sort.split(',').join(' ');
-      // "price avg"
-      this.query = this.query.sort(sort);
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
-      // Default sort
       this.query = this.query.sort('-createdAt');
     }
+
     return this;
   }
 
   limitFields() {
-    // Selecting
     if (this.queryString.fields) {
-      // {fields: 'name,price'}
       const fields = this.queryString.fields.split(',').join(' ');
-      // "name price"
-      this.query = this.query.select(`${fields}`);
+      this.query = this.query.select(fields);
     } else {
-      // Default if there neg "-" before word this meaning excluded it
       this.query = this.query.select('-__v');
     }
+
     return this;
   }
 
-  pagination() {
-    // Pagination
+  paginate() {
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 30;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
+
     this.query = this.query.skip(skip).limit(limit);
+
     return this;
   }
 }
-
 module.exports = APIFeatures;
