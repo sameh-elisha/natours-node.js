3d3bf790de1acafa404f5ce20ef41e327f8b33be
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-08-09 06:24:37 +0200

    updated npm packages and ignored api keys 🚀

--- Controllers/handlerFactory.js
+++ Controllers/handlerFactory.js
@@ -26,6 +26,7 @@ exports.updateOne = Model =>
     if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
+
     res.status(200).json({
       status: 'success',
       data: {
---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+import axios from 'axios';
+import { showAlert } from './alerts';
+import keys from '../../config/keys';
+
+const stripe = Stripe(keys.stripeKey);
+
+export const bookTour = async tourId => {
+  console.log(keys.stripeKey);
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
